333628af162e92556e02ce3b8d8ec1d3aa94deaf
Bartosz Szadkowski <bartoszszadkowski@MacBook-Air-Bartosz.local>
2020-08-12 20:43:43 +0200

    Alarm clock is working, add random link feature

--- alarmClock.py
+++ alarmClock.py
@@ -3,6 +3,11 @@
 import sqlite3
 import pyinputplus as pyip
 from pytube import YouTube
+from datetime import datetime
+import webbrowser
+import sys
+import time
+import random
 
 def printMenu():
     print('---- MAIN MENU ----')
@@ -72,9 +77,41 @@ def deleteEntry():
     except:
         print('Check validity of your ID.')
 
-# create main loop , menu , and interface to adding new links, deleting and listing
+def printAlarmClockMenu():
+    print('---ALARM CLOCK MENU---')
+    print('1. SET TIMER')
+    print('2. TURN ON')
+    print('3. EXIT')
+    alarmClockMenuChoice = pyip.inputChoice(['1', '2', '3'], 'Select choice (1/2/3): ')
+    alarmClockAction = ''
+    if alarmClockMenuChoice == '1':
+        alarmClockAction = 'SET TIMER'
+    elif alarmClockMenuChoice == '2':
+        alarmClockAction = 'TURN ON'
+    elif alarmClockMenuChoice == '3':
+        alarmClockAction = 'EXIT'
+    return alarmClockAction
+
+def turnOnAlarmClock(alarm):
+    if type(alarm) != datetime:
+        print('Alarm clock not set!')
+        return
+    else:
+        checking = True
+        print('ALARM CLOCK TURNED ON')
+        while checking:
+            dateNow = datetime.now()
+            if dateNow.date() == alarm.date() and dateNow.hour == alarm.hour and dateNow.min == alarm.min:
+# HERE ADD RANDOM LINK FEATURE
+                webbrowser.open('https://youtu.be/uCGD9dT12C0?list=RDuCGD9dT12C0')
+                checking = False
+                sys.exit()
+
+            time.sleep(60)
 
 program = True
+
+rowsAmount = 0
 
 # main loop
 while program:
@@ -110,7 +147,18 @@ while program:
                 dbAdministration = False
             elif dbAction == 'DELETE':
                 deleteEntry()
+            # close connection with DB
+            con.close()
 
+    elif action == 'ALARM CLOCK':
+        alarmClockAdministration = True
+        alarm = ''
+        while alarmClockAdministration:
+            alarmClockAction = printAlarmClockMenu() # possible choices: SET TIMER, TURN ON, EXIT
+            if alarmClockAction == 'EXIT':
+                alarmClockAdministration = False
+            elif alarmClockAction == 'SET TIMER':
+                alarm = pyip.inputDatetime('Input date of the alarm: (year/month/day hour:min:sec): ')
+            elif alarmClockAction == 'TURN ON':
+                turnOnAlarmClock(alarm)
 
-        # close connection with DB
-        con.close()
